b85da11f3f79ff8e6ac07cd82fbc80c7e6d73def
Alexandros Kosiaris <akosiaris@gmail.com>
2025-01-23 15:58:36 +0200

    Bump ingress-nginx to 4.12.0
    Update to a more recent version
    
    Signed-off-by: Alexandros Kosiaris <akosiaris@gmail.com>

--- modules/k0s/helm.tf
+++ modules/k0s/helm.tf
@@ -41,7 +41,7 @@ resource "helm_release" "ingress-nginx" {
   repository = "https://kubernetes.github.io/ingress-nginx"
   chart      = "ingress-nginx"
   namespace  = "kube-system"
-  version    = "4.7.0"
+  version    = "4.12.0"
   values = [
     yamlencode(local.ingress_nginx_values)
   ]
