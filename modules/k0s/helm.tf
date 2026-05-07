01f659a4d85a4860c127691dec06d40c60dab149
Alexandros Kosiaris <akosiaris@gmail.com>
2026-05-07 16:53:31 +0300

    chore: Bump hccm to 1.30.1
    Why:
    Past July 1st, 2026, versions older than 1.30.1 will not work as the
    datacenter API property will be deprecated in favor of the location
    property. Keeping older versions around would make the hccm crash
    
    Signed-off-by: Alexandros Kosiaris <akosiaris@gmail.com>

--- modules/k0s/helm.tf
+++ modules/k0s/helm.tf
@@ -13,7 +13,7 @@ resource "helm_release" "hccm" {
   chart      = "hcloud-cloud-controller-manager"
   namespace  = "kube-system"
   # Versioning policy at https://github.com/hetznercloud/hcloud-cloud-controller-manager#versioning-policy
-  version = "1.26.0"
+  version = "1.30.1"
 }
 
 locals {
